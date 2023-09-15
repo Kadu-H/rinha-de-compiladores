e49df5ff93b3fcbebeeda6cdfb4ed8c737d719dd
Kdu <olocgame@gmail.com>
2023-09-15 16:10:49 -0300

    adding next line

--- interpret.js
+++ interpret.js
@@ -1,20 +1,38 @@
-var jscode = "";
-
 function interpret(node){
+    var jscode = "";
     switch(node.kind){
         case "Print":
-            jscode += `console.log(${interpret(node.value)})`;
+            jscode += `console.log(${interpret(node.value)});`;
             break;
         case "Str":
             return `"${node.value}"`;
+        case "Let":
+            jscode += `let ${node.name.text} = ${interpret(node.value)};`;
+            break;
+        case "Var":
+            return node.text;
+        case "Binary":
+            const lhs = interpret(node.lhs);
+            const rhs = interpret(node.rhs);
+            switch(node.op){
+                case "Add":
+                    return `${lhs}+${rhs}`;
+                case "Sub":
+                    return `${lhs}-${rhs}`;
+                default:
+                    break;
+            }
         case "Int":
             return node.value;
         case "Bool":
             return node.value;
         default:
-            return node.value;
+            break;
+    }
+    if(node.next){
+        jscode += interpret(node.next);
     }
     return jscode;
 }
 
-module.export = interpret;
+module.exports = interpret;
