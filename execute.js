23f051b6acfeb42a8cb5fbee54ede6757ccceb7f
Kdu <olocgame@gmail.com>
2023-09-15 16:51:05 -0300

    Adding time execution

--- execute.js
+++ execute.js
@@ -1,4 +1,12 @@
 const json = require("./var/rinha/source.rinha.json");
 const interpret = require("./interpret.js");
 
+const start = performance.now();
+
 eval(interpret(json.expression));
+
+const end = performance.now();
+
+const tempExec = parseFloat((end-start)/1000).toFixed(5)
+
+console.log(`Tempo de execução: ${tempExec}`)
