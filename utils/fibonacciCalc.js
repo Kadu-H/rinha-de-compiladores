5df86d465a0150ea314a974de9f4ef92f90ac50d
Kdu <olocgame@gmail.com>
2023-09-15 20:16:07 -0300

    BigInt in fibonacciCalc

--- utils/fibonacciCalc.js
+++ utils/fibonacciCalc.js
@@ -1,14 +1,10 @@
-function fibonacciCalc(n){
-  	if(n < 1) {
-    	return 0;
+function fibonacciCalc(n) {
+    let a = BigInt(0);
+    let b = BigInt(1);
+    for (let i = BigInt(0); i < n; i++) {
+      [a, b] = [b, a + b];
     }
-    let a = 0;
-    let b = 1;
-    for (let i = 1; i < n; ++i) {
-		let c = a + b;
-      	a = b;
-      	b = c;
-    }
-    return b;
-}
-module.exports = fibonacciCalc;
+    return a;
+  }
+  
+module.exports = fibonacciCalc
