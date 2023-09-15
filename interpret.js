23b42ce5e3f9d195767a198c05e7df1af7a33df5
Kdu <olocgame@gmail.com>
2023-09-15 16:27:07 -0300

    adding condition

--- interpret.js
+++ interpret.js
@@ -1,16 +1,13 @@
 function interpret(node){
     var jscode = "";
+    //console.log(node)
     switch(node.kind){
         case "Print":
             jscode += `console.log(${interpret(node.value)});`;
             break;
-        case "Str":
-            return `"${node.value}"`;
         case "Let":
             jscode += `let ${node.name.text} = ${interpret(node.value)};`;
             break;
-        case "Var":
-            return node.text;
         case "Binary":
             const lhs = interpret(node.lhs);
             const rhs = interpret(node.rhs);
@@ -19,9 +16,28 @@ function interpret(node){
                     return `${lhs}+${rhs}`;
                 case "Sub":
                     return `${lhs}-${rhs}`;
+                case "Lt":
+                    return `${lhs}<${rhs}`;
+                case "Gt":
+                    return `${lhs}>${rhs}`;
+                case "Lte":
+                    return `${lhs}<=${rhs}`;
+                case "Gte":
+                    return `${lhs}>=${rhs}`;
                 default:
                     break;
             }
+        case "If":
+            jscode += `if(${interpret(node.condition)}){
+                ${interpret(node.then)}
+            } else{
+                ${interpret(node.otherwise)}
+            }`
+            break;
+        case "Var":
+            return node.text;
+        case "Str":
+            return `"${node.value}"`;
         case "Int":
             return node.value;
         case "Bool":
